dcb12eedb85b9a590f081059086c8d1ba15a6dc9
yuma <mr.yuma.tanaka@gmail.com>
2022-10-26 21:30:27 -0400

    Simple ProgramOverview

--- pageElements/home/ProgramOverview.tsx
+++ pageElements/home/ProgramOverview.tsx
@@ -4,10 +4,10 @@ import styles from './ProgramOverview.module.css';
 
 export const ProgramOverview: FC = () => {
   return (
-    <div className="mx-xOffset">
-      <div className="rounded-xl border-4 border-black bg-gray-100 px-20 pt-28 pb-36 shadow-solid-lg">
-        <h2 className="text-7xl font-semibold underline">10-Week Program</h2>
-        <p className="mt-20 text-4xl">
+    <div className={styles['container']}>
+      <div className={styles['card'] + ' solid-shadow-animation'}>
+        <h2 className={styles['title'] + ' title-font'}>10-Week Program</h2>
+        <p className="paragraph-font">
           Outliers is a 10-week, summer program to empower exceptional student
           builders in Crypto.
           <br />
@@ -15,7 +15,7 @@ export const ProgramOverview: FC = () => {
           from across the country who come together to learn and build. We
           facilitate education and mentorship from top industry leaders, and
           provide resources to build and scale the{' '}
-          <span className="font-bold">next Web3 breakthrough</span>.
+          <span>next Web3 breakthrough</span>.
         </p>
       </div>
     </div>
--- pageElements/home/SplashScreen.tsx
+++ pageElements/home/SplashScreen.tsx
@@ -5,17 +5,17 @@ import styles from './SplashScreen.module.css';
 export const SplashScreen: FC = () => {
   return (
     <div className={styles['container']}>
-      <div className={styles['names-container'] + ' xl-title'}>
+      <div className={styles['names-container'] + ' title-font'}>
         <p>Floodgate</p>
         <p>Standard Crypto</p>
       </div>
       <div className={styles['main-title']}>
-        <h1 className="xxl-title">Create the Future</h1>
-        <h2 className="xl-title">Builders x Crypto</h2>
+        <h1 className="xxl-title-font">Create the Future</h1>
+        <h2 className="title-font">Builders x Crypto</h2>
       </div>
       <div></div>
       <div>
-        <p className={styles['outliers'] + ' title'}>Outliers</p>
+        <p className={styles['outliers'] + ' subtitle-font'}>Outliers</p>
       </div>
     </div>
   );
